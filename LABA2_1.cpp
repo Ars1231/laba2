c2f7b8087f4afc34b1496fd3e69a9a05ffa7f0be
Ars1231 <84440425+Ars1231@users.noreply.github.com>
2024-10-05 14:13:14 +0300

    Update LABA2_1.cpp

--- LABA2_1.cpp
+++ LABA2_1.cpp
@@ -5,36 +5,66 @@ using namespace std;
 int main() {
 
 	int X, Y, Z;
-	cin>>X>>Y>>Z;
-	//каждое из чисел X и Y нечетное;
-	if (X % 2 != 0 && Y % 2 != 0) cout<<"condition is true"<<endl;
+	cout << "1 - проверка каждое из чисел X и Y нечетное; " << endl;
+	cout << "2 - только одно из чисел X и Y меньше 20; " << endl;
+	cout << "3 - хотя бы одно из чисел X и Y равно нулю; " << endl;
+	cout << "4 - каждое из чисел X, Y, Z отрицательное; " << endl;
+	cout << "5 - только одно из чисел X, Y и Z кратно пяти; " << endl;
+	cout << "6 - хотя бы одно из чисел X, Y, Z больше 100; " << endl;
+	cout << "0 -выход из программы. " << endl;
 
-	else cout<<"condition is false"<<endl;
+	while(true){
 
-	//только одно из чисел X и Y меньше 20;
-	if ((X < 20 && Y >= 20) || (X >= 20 && Y < 20)) cout<<"condition is true"<<endl;
+	cin >> X >> Y >> Z;
 
-	else cout<<"condition is false"<<endl;
-
-	//хотя бы одно из чисел X и Y равно нулю;
-    	if (X == 0 || Y == 0) cout<<"condition is true"<<endl;
-
-  	else cout<<"condition is false"<<endl;
-
-	//каждое из чисел X, Y, Z отрицательное;
-   	if (X < 0 && Y < 0 && Z < 0) cout<<"condition is true"<<endl;
-
-    	else cout<<"condition is false"<<endl;
+	switch(x){
+	case 1:
+		//каждое из чисел X и Y нечетное;
+		if (X % 2 != 0 && Y % 2 != 0) cout << "condition is true" << endl;
+	
+		else cout << "condition is false" << endl;
 		
-	//только одно из чисел X, Y и Z кратно пяти;
-    	if ( (X % 5 == 0 && Y % 5 != 0 && Z % 5 != 0) || (X % 5 != 0 && Y % 5 == 0 && Z % 5 != 0) || (X % 5 != 0 && Y % 5 != 0 && Z % 5 == 0) ) cout<<"condition is true"<<endl;
+		break;
+	case 2:
+		//только одно из чисел X и Y меньше 20;
+		if ((X < 20 && Y >= 20) || (X >= 20 && Y < 20)) cout << "condition is true" << endl;
+	
+		else cout << "condition is false" << endl;
 
-   	else cout<<"condition is false"<<endl;
-		
-	//хотя бы одно из чисел X, Y, Z больше 100.
-   	if (X > 100 || Y > 100 || Z > 100) cout<<"condition is true"<<endl;
+		break;
+	case 3:
+		//хотя бы одно из чисел X и Y равно нулю;
+	    	if (X == 0 || Y == 0) cout << "condition is true" << endl;
+	
+	  	else cout << "condition is false" << endl;
 
-   	else cout<<"condition is false"<<endl;
+		break;
+	case 4:
+		//каждое из чисел X, Y, Z отрицательное;
+	   	if (X < 0 && Y < 0 && Z < 0) cout << "condition is true" << endl;
+	
+	    	else cout << "condition is false" << endl;
 
+		break;
+	case 5:
+		//только одно из чисел X, Y и Z кратно пяти;
+	    	if ( (X % 5 == 0 && Y % 5 != 0 && Z % 5 != 0) || (X % 5 != 0 && Y % 5 == 0 && Z % 5 != 0) || (X % 5 != 0 && Y % 5 != 0 && Z % 5 == 0) ) cout << "condition is true" << endl;
+	
+	   	else cout << "condition is false" << endl;
+
+		break;
+	case 6:
+		//хотя бы одно из чисел X, Y, Z больше 100.
+	   	if (X > 100 || Y > 100 || Z > 100) cout << "condition is true" << endl;
+	
+	   	else cout << "condition is false" << endl;
+
+		break;
+	case 7:
+		return 0;
+	
+	}
+	
+	}
 	return 0;
 }
