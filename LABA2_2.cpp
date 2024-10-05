c0c2e1764b46c33f3680477b964bcef93b013676
Ars1231 <84440425+Ars1231@users.noreply.github.com>
2024-10-05 13:37:01 +0300

    Update LABA2_2.cpp

--- LABA2_2.cpp
+++ LABA2_2.cpp
@@ -10,34 +10,59 @@ int main() {
 	using byte = unsigned char;
 	pair<byte, byte> positionA;
 	pair<byte, byte> positionB;
+    int x;
+ 
     
-
+    while (true){
+ 
     cout << "Enter position A (a b): ";
     cin >> positionA.first >> positionA.second;
 
-    cout << "Enter position B (c d): ";
+    cout << "Enter position B (c d): " << "\n";
     cin >> positionB.first >> positionB.second;
-    cout << "\n";
+    cin>>x;
     
-    if (positionA.first == positionB.first || positionA.second == positionB.second) cout << "Threatening" << endl;
-    else cout << "Not threatening" << endl;
+    switch(x){
     
-    if ( positionA.first - positionA.second == positionB.first - positionB.second ) cout << "Threatening" << endl;
-    else cout << "Not threatening" << endl;
+    case 1:
+	    if (positionA.first == positionB.first || positionA.second == positionB.second) cout << "Threatening" << endl;
+	    else cout << "Not threatening" << endl;
+	    break;
+	    
     
-    if ( ( abs(positionA.first - positionB.first) <= 1 ) && ( abs(positionA.second - positionB.second) <= 1 ) ) cout << "Threatening" << endl;
-    else cout << "Not threatening" << endl;
+    case 2:
+	    if ( positionA.first / positionA.second == positionB.first  / positionB.second ) cout << "Threatening" << endl;
+	    else cout << "Not threatening" << endl;
+	   break;
+
     
-    if ( (positionA.first == positionB.first || positionA.second == positionB.second) || ( (positionA.first / positionB.first) == (positionA.second / positionB.second) ) ) cout << "Threatening" << endl;
-    else cout << "Not threatening" << endl;
+    case 3:
+	    if ( ( abs(positionA.first - positionB.first) <= 1 ) && ( abs(positionA.second - positionB.second) <= 1 ) ) cout << "Threatening" << endl;
+	    else cout << "Not threatening" << endl;
+	   break;
+
     
-    if ( (positionA.first == positionB.first) && (positionB.second - positionA.second == 1) ) cout << "Threatening 'first condition'" << endl;
-    else cout << "Not threatening 'first condition'" << endl;
+    case 4:
+	    if ( (positionA.first == positionB.first || positionA.second == positionB.second) || ( (positionA.first / positionB.first) == (positionA.second / positionB.second) ) ) cout << "Threatening" << endl;
+	    else cout << "Not threatening" << endl;
+	    break;
+
+    case 5:
+	    if ( (positionA.first == positionB.first) && (positionB.second - positionA.second == 1) ) cout << "Threatening 'first condition'" << endl;
+	    else cout << "Not threatening 'first condition'" << endl;
+	 break;
     
-     if ( (positionA.first < positionB.first) && (abs(positionA.first - positionB.first) == 1) ) cout << "Threatening 'second condition'" << endl;
-    else cout << "Not threatening 'second condition'" << endl;
+    case 6:
+	    if ( (positionA.first < positionB.first) && (abs(positionA.first - positionB.first) == 1) ) cout << "Threatening 'second condition'" << endl;
+	    else cout << "Not threatening 'second condition'" << endl;
+	    break;
+	    
+    case 7:
+    	return 0;
+  
+    }
     
-    
+    }
     
     return 0;
 }
