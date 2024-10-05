a81603690169febb89167962796357272bb3c331
Ars1231 <84440425+Ars1231@users.noreply.github.com>
2024-10-05 16:49:34 +0300

    Update LABA2_3.cpp

--- LABA2_3.cpp
+++ LABA2_3.cpp
@@ -6,21 +6,23 @@ using namespace std;
 int main() {
 	int N;
 	while (true){
-	cout << "Введите число N" << endl;
-	cin >> N;	
-	cout << "Чтобы выйти, введите 0" << endl;
-	switch(N){
-		case 0:
-			return 0;
-		default:
-			cout << "Таблица умножения для числа "<< N << endl;
-		    for (int i = 1; i < 10; i++) {
-		    cout << i << " x "<< N  << " = " << N * i << endl;
-		    
-    }
-
-	}
+		
+		cout << "Введите число N" << endl;
+		cin >> N;	
+		cout << "Чтобы выйти, введите 0" << endl;
+		
+		switch(N){
+			case 0:
+				return 0;
+			default:
+				cout << "Таблица умножения для числа "<< N << endl;
+				
+			        for (int i = 1; i < 10; i++) {
+			        cout << i << " x "<< N  << " = " << N * i << endl;
+			        }
 	
-	}
+		          }
+	
+	             }
     return 0;
 }
